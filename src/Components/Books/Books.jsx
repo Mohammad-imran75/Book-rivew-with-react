b0af77d3c470055acf98477e3b14585388567500
Mohammad-imran75 <imranbd7531@gmail.com>
2024-03-28 23:08:21 +0600

    Home page responsive complete

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -9,7 +9,7 @@ const books = useLoaderData();
       <div>
         <h2 className="text-[40px] text-center font-bold mt-10">Books </h2>
       </div>
-      <div className="grid grid-cols-3 gap-6 font-WorkSans ">
+      <div className="lg:grid grid-cols-3 gap-6 font-WorkSans ">
         {books.map((book) => (
           <Book book={book} key={book.id}></Book>
         ))}
