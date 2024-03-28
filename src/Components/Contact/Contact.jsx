1eee4f55c5f33e32609b0a5737161cfce9f6523a
Mohammad-imran75 <imranbd7531@gmail.com>
2024-03-28 22:24:30 +0600

    shapBar chart completed

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,11 +1,25 @@
-
-
+import { MdWifiCalling1 } from "react-icons/md";
+import { TbBrandGmail } from "react-icons/tb";
 const Contact = () => {
-    return (
-        <div>
-            <h1>This is contact page</h1>
+  return (
+    <div className="card card-side lg: max-w-5xl p-10 border-2 mx-auto bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://i.ibb.co/68YgZkn/kindpng-81724-1.png"
+          alt="Movie"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-4xl">CONTACT ME OUR PAGE!!</h2>
+        <p className="flex items-center gap-3"><MdWifiCalling1></MdWifiCalling1>++9703634509238</p>
+        <p className="flex items-center gap-3"><TbBrandGmail></TbBrandGmail>hablubaba@gmail.com</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">See more</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default Contact;<h1>This is contact page</h1>
+export default Contact;
+<h1>This is contact page</h1>;
--- src/Components/Pages/Pages.jsx
+++ src/Components/Pages/Pages.jsx
@@ -1,36 +1,71 @@
 import { useEffect, useState } from "react";
 import { getBookAppliction } from "../../utility/localStorage";
 import { useLoaderData } from "react-router-dom";
-import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
-// import ShapeChart from "../ShapeChart/ShapeChart";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis,Cell } from "recharts";
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${
+    y + height / 3
+  }
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+    x + width
+  }, ${y + height}
+  Z`;
+};
 
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
 
 const Pages = () => {
-  const [readPages ,setReadPages ] = useState([]);
+  const [readPages, setReadPages] = useState([]);
   const books = useLoaderData();
+  console.log(books)
   const loadReadPages = getBookAppliction();
-  let storeReadBooks = [];
-  useEffect(()=>{
-    if(books.length > 0){
-      const readListBooks = books.filter(book => loadReadPages.includes(book.bookId));
-      storeReadBooks.push(readListBooks)
-      setReadPages(storeReadBooks)
-    }
+  console.log(loadReadPages)
+  
+  useEffect(() => {
     
-  },[])
+    if (books.length > 0) {
+      let storeReadBooks = [];
+      const readListBooks = books.filter((book) =>
+        loadReadPages.includes(book.bookId)
+      );
+      // storeReadBooks.push(readListBooks);
+      setReadPages(readListBooks);
+      console.log(readListBooks);
+    }
+
+  }, [books]);
   console.log(readPages);
+
   return (
-    <div className="font-WorkSans">
-    <BarChart width={1000} height={600} data={readPages[0]}>
-            <Bar dataKey="totalPages" fill="green" />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="name" />
-            <YAxis />
-        </BarChart>
-  </div>
-);
+    <div className="font-WorkSans flex flex-col items-center">
+      <BarChart
+      width={500}
+      height={300}
+      data={readPages}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="bookName" />
+      <YAxis />
+      <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+        {readPages.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+        ))}
+      </Bar>
+    </BarChart>
+    </div>
+  );
 };
 
 export default Pages;
-
-
